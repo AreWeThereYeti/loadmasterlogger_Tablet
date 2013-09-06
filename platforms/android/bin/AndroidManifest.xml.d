c6f223673b867ea9d793ea2e6653e89430c5c695
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Air.local>
2013-09-06 22:05:20 +0200

    FUCKUP

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/bin/AndroidManifest.xml : \
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/bin/HelloWorld.ap_ \
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/bin/classes.dex \
+/Users/mikkelmarkvardsen/LoadmasterLogger/platforms/android/libs/cordova-3.0.0.jar \
