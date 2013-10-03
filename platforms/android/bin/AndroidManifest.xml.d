66aa9e9f49301fdcf36b047564fdfb757113c573
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Air.local>
2013-10-03 11:11:32 +0200

    fuckery

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mikkelmarkvardsen/loadmasterlogger_Tablet/platforms/android/bin/AndroidManifest.xml : \
+/Users/mikkelmarkvardsen/loadmasterlogger_Tablet/platforms/android/AndroidManifest.xml \
