effafbe444a1bdd41da1c623db5343dc384b4bb4
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Air.local>
2013-10-29 11:16:09 +0100

    small changes

--- www/src/js/index.js
+++ www/src/js/index.js
@@ -72,9 +72,7 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 		*/
-		
-        console.log('Received Event: ' + id);
-    }
+	    }
 };
 
 /*
